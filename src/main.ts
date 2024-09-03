4ee38b75dcbdcc02d7eeb9219308793139102f0f
Mukul Kathait <kathaitmukul0699@gmail.com>
2024-09-03 10:09:59 +0530

    main.ts aligned for deployment

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://weather-buddy-lake.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  app.use(helmet());
+
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // Limit each IP to 100 requests per windowMs
+    }),
+  );
+
   app.use(
     session({
       secret: process.env.SESSION_SECRET,
@@ -19,6 +33,7 @@ async function bootstrap() {
       },
     }),
   );
+
   app.use(passport.initialize());
   app.use(passport.session());
   await app.listen(3000);
