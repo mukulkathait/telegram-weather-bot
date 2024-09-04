2377ac001bd66d60af23f573531a75d4dd612734
Mukul Kathait <kathaitmukul0699@gmail.com>
2024-09-04 10:32:30 +0530

    main file updated

--- public/main.js
+++ public/main.js
@@ -5,15 +5,14 @@ const app_module_1 = require("./app.module");
 const session = require("express-session");
 const passport = require("passport");
 const helmet_1 = require("helmet");
-const express_rate_limit_1 = require("express-rate-limit");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors({});
+    app.enableCors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true,
+    });
     app.use((0, helmet_1.default)());
-    app.use((0, express_rate_limit_1.default)({
-        windowMs: 15 * 60 * 1000,
-        max: 100,
-    }));
     app.use(session({
         secret: process.env.SESSION_SECRET,
         saveUninitialized: false,
--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,30 @@ import * as session from 'express-session';
 import * as passport from 'passport';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Access the underlying Express instance
+  // const expressApp = app.getHttpAdapter().getInstance();
+
+  // expressApp.set('trust proxy', true);
+
   app.enableCors({
-    // origin: 'https://weather-buddy-lake.vercel.app/',
-    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    // credentials: true,
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
   });
 
   app.use(helmet());
 
-  app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // Limit each IP to 100 requests per windowMs
-    }),
-  );
+  // app.use(
+  //   rateLimit({
+  //     windowMs: 15 * 60 * 1000, // 15 minutes
+  //     max: 100, // Limit each IP to 100 requests per windowMs
+  //   }),
+  // );
 
   app.use(
     session({
